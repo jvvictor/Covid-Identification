91a6a096ecd25fa65e93a72a445cf8863e628744
jvvictor <jvsoares2010@hotmail.com>
2021-06-09 16:54:56 -0300

    EDA modulated version

--- eda.py
+++ eda.py
@@ -126,15 +126,6 @@ class Preprocess:
         return
 
 
-
-
-
-
-
-
-
-
-
 if __name__ == "__main__":
 
     data_path = "C:/Users/jvsoa/Desktop/COVID/metadata.csv"
@@ -145,83 +136,4 @@ if __name__ == "__main__":
     covid_1.show_image(int(input("How many images to show?: ")))
     covid_1.transf_binary()
     covid_1.split_train_data()
-
-    print(covid_1.traindata.head(5))
-    print(covid_1.valdata.head(5))
     
-    '''
-    metadata = pd.read_csv("../metadata.csv", header=0)
-    for n in range(metadata.shape[0]):
-        if metadata['modality'][n] != "X-ray":
-            metadata = metadata.drop([n], axis=0)
-    
-    metadata.reset_index(drop = True, inplace = True)
-    
-    for j in range(metadata.shape[0]):
-        if search('.png', metadata['filename'][j]):
-            pass
-        elif search('.jpg', metadata['filename'][j]):
-            pass
-        elif search('.jpeg', metadata['filename'][j]):
-            pass
-        elif search('.JPG', metadata['filename'][j]): 
-            pass
-        else:
-            metadata = metadata.drop([j], axis=0)
-
-    metadata.reset_index(drop = True, inplace = True)
-
-    want = ["patientid","finding","modality","filename"]
-    metadata = metadata[want]
- 
-    listdis = [metadata['finding'][0]]
-
-    for k in range(metadata.shape[0]):
-        if metadata['finding'][k] not in listdis:
-            listdis.append(metadata['finding'][k])
-    
-    print("")
-    print("The diseases are:", listdis )
-    print("The number os diseases is:", len(listdis))
-
-    listpatient = [metadata['patientid'][0]]
-
-    for p in range(metadata.shape[0]):
-        if metadata['patientid'][p] not in listpatient:
-            listpatient.append(metadata['patientid'][p])
-
-    print("")
-    print("The number of patients is:", len(listpatient))
-
-    cv2.namedWindow("T", 0)
-
-    for l in range(5):
-        img = cv2.imread(os.path.join('../images', metadata['filename'][l]), 1)
-        cv2.imshow('T', img)
-        cv2.waitKey(0)
-
-    for j in range(metadata.shape[0]):
-        if metadata['finding'][j] != "COVID-19":
-            metadata['finding'][j] = 0
-        else:
-            metadata['finding'][j] = 1
-
-    unique_patients = metadata['patientid'].unique()
-    train , test = train_test_split(unique_patients, test_size = 0.05, random_state = 0, shuffle = True)
-
-    traindata = metadata.copy()
-    valdata = metadata.copy()
-
-    for i in range(metadata.shape[0]):
-        if metadata['patientid'][i] not in test:
-            traindata = traindata.drop([i], axis = 0)
-        else:
-            valdata = valdata.drop([i], axis = 0)
-
-    traindata.reset_index(drop = True, inplace = True)
-    valdata.reset_index(drop = True, inplace = True)
-
-    print("")
-    print("Work complete")
-
-    '''
